94d7a1dd06a2adf1458ae7df64800926b8366618
lpj <200814174@qq.com>
2018-12-11 11:52:35 +0800

    change phone size

--- control/v-base.js
+++ control/v-base.js
@@ -2,15 +2,7 @@
 var app = new Vue({
 	el: '#app',
 	data() {
-		return {
-			h_menu: h_menu,
-			v_menu: f_menu,
-			p_title: '公司簡介',
-			des: about_description,
-			showPhoneMenu: false,
-			show_sub_menu_index: false,
-			isPhone: true
-		}
+		return pdata
 	},
 	mounted() {
 		if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
